4e22b792cb79ee5257dc284603c0610c844127df
MadDeveloper <julien.sergent46@orange.fr>
2016-07-20 00:08:15 +0200

    Change .gitignore

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import 'semantic-ui'
+import 'reflect-metadata'
+import 'zone.js/dist/zone'
+import { bootstrap }    from 'angular2/platform/browser'
+import { AppComponent } from './app.component'
+
+bootstrap( AppComponent )
